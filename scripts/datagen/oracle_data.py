7576280439c1242053df20634d3cad525d34025e
dataintern4cv <cristian-m.vicol@endava.com>
2023-11-17 18:41:51 +0200

    Tunnel and session exit problem solved

--- scripts/datagen/oracle_data.py
+++ scripts/datagen/oracle_data.py
@@ -1,1 +1,108 @@
 # Generator script for Oracle
+import faker
+import logging
+import os
+from faker import Faker
+from sqlalchemy.orm import sessionmaker
+from scripts.init_config import config
+from scripts.utils.database_utils import get_ssh_tunnel, create_session, get_engine_path
+from sqlalchemy import exc, text, create_engine, bindparam
+
+fake = Faker()
+
+def gen_data_fake_regions(nr_recs: int):
+
+    data_list = []
+    for i in range(5, nr_recs):
+        data = {}  # Create a new dictionary in each iteration
+        data['region_id'] = i
+        data['region_name'] = f'r_{fake.unique.word()}'
+        data_list.append(data)
+    return data_list
+
+def insert_data_into_regions():
+    data_list = gen_data_fake_regions(10)
+
+    config.setup_logging(logs='info')
+    log = logging.getLogger(os.path.basename(__file__))
+
+    # Initialize tunnel and session to None
+    tunnel = None
+    session = None
+
+    tunnel = get_ssh_tunnel(service='ORACLE')
+
+    tunnel.start()
+    local_port = str(tunnel.local_bind_port)
+    session = create_session(db='ORACLE', local_port=local_port)
+
+    sql_query = text("INSERT INTO REGIONS (REGION_ID, REGION_NAME) VALUES (:region_id, :region_name)")
+    sql_query = sql_query.bindparams(bindparam('region_id'), bindparam('region_name'))
+    for r in data_list:
+        # Execute the query for each row, passing values as a dictionary
+        session.execute(sql_query, r)
+
+
+    # commit the changes
+    session.commit()
+    session.close()
+    tunnel.stop()
+
+
+def insert_data_into_regions__2():
+    data_list = gen_data_fake_regions(10)
+
+    config.setup_logging(logs='info')
+    log = logging.getLogger(os.path.basename(__file__))
+
+    # Initialize tunnel and session to None
+    tunnel = None
+    session = None
+
+    try:
+        # create tunnel
+        tunnel = get_ssh_tunnel(service='ORACLE')
+        tunnel.start()
+        local_port = str(tunnel.local_bind_port)
+
+        try:
+            # create engine and session
+            engine_path_auth = get_engine_path('ORACLE', local_port)
+            engine = create_engine(engine_path_auth)
+            Session = sessionmaker(bind=engine)
+
+            with Session() as session:
+                try:
+                    # Define the SQL query with bind parameters
+                    sql_query = text("INSERT INTO REGIONS (REGION_ID, REGION_NAME) VALUES (:region_id, :region_name)")
+                    sql_query = sql_query.bindparams(bindparam('region_id'), bindparam('region_name'))
+
+                    for r in data_list:
+                        # Execute the query for each row, passing values as a dictionary
+                        session.execute(sql_query, r)
+
+                    # commit the changes
+                    session.commit()
+
+                except exc.SQLAlchemyError as e:
+                    log.error(f'Error executing SQL query: {e}', exc_info=True)
+
+            print('session')
+        except Exception as e:
+            log.error(f'Error setting up database connection: {e}', exc_info=True)
+
+    finally:
+        print('tunnel')
+        print(tunnel.__str__())
+        if tunnel:
+            tunnel.stop()
+
+
+if __name__ == '__main__':
+
+    #data = gen_data_fake_regions(100)
+   # print(data)
+
+    insert_data_into_regions__2()
+
+
--- test/test_database_utils.py
+++ test/test_database_utils.py
@@ -1,35 +1,55 @@
 import unittest
-from scripts.utils.database_utils import get_ssh_tunnel, create_session
-from sshtunnel import SSHTunnelForwarder
-from sqlalchemy import exc, text
+import logging
+import os
+
+from scripts.init_config import config
+from scripts.utils.database_utils import get_ssh_tunnel, create_session, get_engine_path
+from sqlalchemy import exc, text, create_engine
+
+
+
+
 
 
 class test_database_utils(unittest.TestCase):
 
+
     def test_get_ssh_tunnel_oracle(self):
+
+        config.setup_logging(logs='info')
+        log = logging.getLogger(os.path.basename(__file__))
 
         orc_tunnel = get_ssh_tunnel('ORACLE')
         try:
             orc_tunnel.start()
+            log.info('Tunnel start %s! ', orc_tunnel.tunnel_is_up)
         except Exception as e:
-            print(f'Error starting the tunnel: {e}')
+            log.error(f'Error starting the tunnel: {e}')
         finally:
             orc_tunnel.close()
 
     def test_get_ssh_tunnel_postgre(self):
+        config.setup_logging(logs='info')
+        log = logging.getLogger(os.path.basename(__file__))
 
         pg_tunnel = get_ssh_tunnel('POSTGRE')
         try:
             pg_tunnel.start()
+            log.info('Tunnel start %s! ', pg_tunnel.tunnel_is_up)
         except Exception as e:
-            print(f'Error starting the tunnel: {e}')
+            log.error(f'Error starting the tunnel: {e}')
         finally:
             pg_tunnel.close()
 
     def test_session_oracle(self):
+
+        config.setup_logging(logs='info')
+        log = logging.getLogger(os.path.basename(__file__))
+
         # Initialize tunnel and session to None
         tunnel = None
         session = None
+
 
         try:
             # create tunnel
@@ -41,11 +61,13 @@ class test_database_utils(unittest.TestCase):
             session = create_session(db='ORACLE', local_port=local_port)
 
             # test connection
-            test = session.execute(text('select * from regions'))
+            test = session.execute(text('select table_name from user_tables'))
             for row in test:
                 print(row)
+
+
         except exc.SQLAlchemyError as e:
-            print(f'Error connecting to the database: {e}')
+            log.error(f'Error connecting to the database: {e}')
         finally:
             # Close the session and stop the tunnel in the finally block
             if session is not None:
@@ -54,6 +76,9 @@ class test_database_utils(unittest.TestCase):
                 tunnel.stop()
 
     def test_session_postgre(self):
+        config.setup_logging(logs='info')
+        log = logging.getLogger(os.path.basename(__file__))
+
         # Initialize tunnel and session to None
         tunnel = None
         session = None
@@ -72,10 +97,45 @@ class test_database_utils(unittest.TestCase):
             for row in test:
                 print(row)
         except exc.SQLAlchemyError as e:
-            print(f'Error connecting to the database: {e}')
+            log.error(f'Error connecting to the database: {e}')
         finally:
             # Close the session and stop the tunnel in the finally block
             if session is not None:
                 session.close()
             if tunnel is not None:
                 tunnel.stop()
+
+    def test_with_connection(self):
+        conn = None
+        tunnel = None
+
+        try:
+            tunnel = get_ssh_tunnel(service='ORACLE')
+            tunnel.start()
+            local_port = str(tunnel.local_bind_port)
+
+            engine_path_auth = get_engine_path(db='ORACLE', local_port=local_port)
+            print(engine_path_auth)
+
+            engine = create_engine(engine_path_auth)
+
+            """with engine.connect() as conn:
+                result = conn.execute(text('SELECT * FROM regions'))
+                for row in result:
+                    print(row)"""
+            # Connect to the database
+            conn = engine.connect()
+
+            # Execute the query
+            result = conn.execute(text('SELECT * FROM regions'))
+
+            # Fetch and print the results
+            for row in result:
+                print(row)
+
+
+        except Exception as e:
+            print(f"Error: {e}")
+        finally:
+            conn.close()
+            tunnel.close()
